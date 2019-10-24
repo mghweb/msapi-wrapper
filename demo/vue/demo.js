df567e32d2d3ebae5b334aff38f8444fc892a1e8
Max Hegler <maxghegler@gmail.com>
2019-10-24 09:34:27 -0700

    updated demo

--- demo/vue/demo.js
+++ demo/vue/demo.js
@@ -1,51 +1,113 @@
 (function(){
 
-	function SearchApp( endpoint, defaultSearchParams ) {
-
-		this.data = {};
-		this.response = {};
-		this.requestParams = {};
-
-		// define search api
-		this.api = new MSAPI.Client( endpoint, ( defaultSearchParams || {} ) );
-
-		// bind search event
-		this.api.on( 'search', function( data, response, requestParams ) {
-
-			// define instance properties on search callback
-			this.data = data;
-			this.response = response;
-			this.requestParams = requestParams;
-
-		}.bind( this ));
-
-		// run default search
-		this.api.search();
-
-	}
-
-	var searchApp = new SearchApp( __CONFIG__.endpoint );
+	// define search API instance
+	var searchApi = new MSAPI.Client( __CONFIG__.endpoint, { Per_Page: 25 } );
 
 	// ================================================== //
 
 	Vue.component(
-		'search-results',
+		'search-app',
 		{
 			data: function() {
 				return {
 					searchData: {}
 				}
 			},
-			watch: {
-				searchData: function() {
-					return ( searchApp && searchApp.data ) || {};
+			created: function() {
+
+				// bind event listener
+				searchApi.on( 'search', ( data ) => { this.searchData = data; } );
+
+				// run default search
+				searchApi.search();
+
+			},
+			template: `
+				<div>
+					<div class="row">
+						<div class="col stretch">
+							<search-bar></search-bar>
+						</div>
+					</div>
+					<div class="row">
+						<aside class="col">
+							<search-facets :facets="searchData.facets"></search-facets>
+						</aside>
+						<main class="col stretch">
+							<search-results :results="searchData.results"></search-results>
+							<search-pagination :pagination="searchData.pagination"></search-pagination>
+						</main>
+					</div>
+				</div>
+			`
+		}
+	);
+
+	Vue.component(
+		'search-bar',
+		{
+			data: function() {
+				return {
+					query: '%'
+				}
+			},
+			methods: {
+				executeSearchQuery: function( $event ) {
+
+					$event.preventDefault();
+
+					searchApi.query( this.query ).search();
+
+				}
+			},
+			template: `
+				<div>
+					<strong>Search Bar:</strong>
+					<form @submit="executeSearchQuery( $event );">
+						<input type="text" placeholder="Search..." v-model="query" />
+						<button>Go</button>
+					</form>
+				</div>
+			`
+		}
+	);
+
+	Vue.component(
+		'search-facets',
+		{
+			props: {
+				facets: {
+					type: Array,
+					default: () => { return [] }
+				}
+			},
+			template: `
+				<div>
+					<strong>Search Facets:</strong>
+					<ol>
+						<li v-for="facet in facets">
+							{{ facet.name }}
+						</li>
+					</ol>
+				</div>
+			`
+		}
+	);
+
+	Vue.component(
+		'search-results',
+		{
+			props: {
+				results: {
+					type: Array,
+					default: () => { return [] }
 				}
 			},
 			template: `
 				<div>
 					<strong>Search Results:</strong>
 					<ol>
-						<li v-for="result in searchData.results">
+						<li v-for="result in results">
 							{{ result.name }}
 						</li>
 					</ol>
@@ -57,23 +119,16 @@
 	Vue.component(
 		'search-pagination',
 		{
-			data: function() {
-				return {
-					searchData: {}
-				}
-			},
-			watch: {
-				searchData: function() {
-
-					console.log( searchApp );
-
-					return ( searchApp && searchApp.data ) || {};
+			props: {
+				pagination: {
+					type: Object,
+					default: () => { return {} }
 				}
 			},
 			methods: {
 				page: function( n ) {
 	
-					searchApp.api.page( n ).search();
+					searchApi.page( n ).search();
 
 				}
 			},
@@ -81,9 +136,9 @@
 				<div>
 					<hr>
 					<strong>Pages:</strong>
-					<ul>
-						<li v-for="n in searchData.pagination && searchData.pagination.total_pages">
-							<a @click="page( n )">{{ n }}</a>
+					<ul class="inline-list">
+						<li v-for="n in pagination.total_pages">
+							<a @click="page( n )" :class="{ red: ( n == pagination.current_page ) }">{{ n }}</a>
 						</li>
 					</ul>
 				</div>
@@ -92,7 +147,7 @@
 	);
 
 	// ================================================== //
-	
+
 	var MSAPIDemo = new Vue({
 		el: '#MSAPIDemo'
 	});
